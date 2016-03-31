f5fb2c64ef6a1099497bb18f8fa040a8789a53df
MGromov <maksim_gromov_2012@mail.ru>
2016-04-01 00:16:25 +0300

    Is hit walls? is hit tail?

---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        List<Figure> wallsList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallsList = new List<Figure>();
+
+            HorizontalLine upLine = new HorizontalLine(0, mapWidth - 2, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, mapWidth - 2 , mapHeight - 1, '+');
+            VerticalLine leftLine = new VerticalLine(0, mapHeight - 1, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
+
+            wallsList.Add(upLine);
+            wallsList.Add(downLine);
+            wallsList.Add(leftLine);
+            wallsList.Add(rightLine);
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallsList)
+                if (wall.IsHit(figure))
+                    return true;
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach (var wall in wallsList)
+                wall.Draw();
+        }
+    }
+}
