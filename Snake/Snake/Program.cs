bb1682d669c3de01bea56631a53ddf96303fd2d3
MGromov <maksim_gromov_2012@mail.ru>
2016-03-31 19:56:01 +0300

    HorizontalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,9 @@ namespace Snake
             Point p2 = new Point(3, 4, '#');
             p2.Draw();
 
+            HorizontalLine line = new HorizontalLine(5, 10, 10, '#');
+            line.Draw();
+
             Console.ReadLine();
         }
     }
